472e9fe3e927878cb5d82192a4a5854eef0d66e0
Salman Ahmed <libra.salman94@gmail.com>
2020-10-18 19:44:27 +0500

    products page done

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -2,28 +2,28 @@ $(function() {
     var num;
 
     $('.button-count:first-child').click(function(){
-      num = parseInt($('.number-product').val());
+      num = parseInt($(this).siblings('.number-product').val());
       if (num > 1) {
-        $('.number-product').val(num - 1);
+        $(this).siblings('.number-product').val(num - 1);
       }
-      if (num == 2) {
-        $('.button-count:first-child').prop('disabled', true);
+      if ($(this).siblings('.number-product').val() == 2) {
+        $(this).siblings('.button-count.first').prop('disabled', true);
       }
       if (num == 99) {
-        $('.button-count:last-child').prop('disabled', false);
+        $(this).siblings('.button-count.last').prop('disabled', false);
       }
     });
     
     $('.button-count:last-child').click(function(){
-      num = parseInt($('.number-product').val());
+      num = parseInt($(this).siblings('.number-product').val());
       if (num < 99) {
-        $('.number-product').val(num + 1);
+        $(this).siblings('.number-product').val(num + 1);
       }
-      if (num > 0) {
-        $('.button-count:first-child').prop('disabled', false);
+      if ($(this).siblings('.number-product').val() > 0) {
+        $(this).siblings('.button-count.first').prop('disabled', false);
       }
       if (num == 98) {
-        $('.button-count:last-child').prop('disabled', true);
+        $(this).siblings('.button-count.first').prop('disabled', true);
       }
     });
     
